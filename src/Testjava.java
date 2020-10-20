b61aafa0c316a1b70c96a596ce340af6451b517b
QSP <QSP@DESKTOP-EVK9GMC>
2020-10-20 09:05:30 +0530

    Day-Tokens

---
+++ src/Testjava.java
@@ -0,0 +1,13 @@
+
+public class Testjava {
+
+	public static void main(String[] args) {
+		System.out.print(true);
+		System.out.print("Hello welcome to jva");
+        System.out.print('A');
+        System.out.print(20);
+        System.out.print(30.90);
+
+	}
+
+}
---
+++ src/Testjava1.java
@@ -0,0 +1,10 @@
+
+public class Testjava1 {
+	public static void main(String[] args) {
+		System.out.println(20+20);
+		System.out.println("The value is"+20+20);
+	    System.out.println(20+20+"The value is");
+		System.out.println("The value is"+(20+20));
+	}
+
+}
