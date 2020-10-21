9a44070643795188069a3474b03e2d9ff05d4b3e
QSP <QSP@DESKTOP-EVK9GMC>
2020-10-21 09:00:14 +0530

    Varaibles

---
+++ src/varaibles/Demo1.java
@@ -0,0 +1,21 @@
+package varaibles;
+
+public class Demo1 {
+	public static void main(String[] args) {
+		String name="John";
+		String DOB="29-1-1238";
+		double salary=5678.0987654;
+		int empID=1234;
+		String companyName="qwerty";
+		
+//		System.out.println("My name is "+name);
+//		System.out.println("My DOB is "+DOB);
+//		System.out.println("My salary is "+salary);
+//		System.out.println("Emp id is "+empID);
+//		System.out.println("My companyname is "+companyName);
+		
+System.out.println("My name is "+name+" "+DOB+" "+salary+" "+empID+" "+companyName);		
+		
+	}
+
+}
---
+++ src/varaibles/Demo3.java
@@ -0,0 +1,17 @@
+package varaibles;
+
+public class Demo3 {
+	//golbal varabile
+static int b=20;
+
+
+	public static void main(String[] args) {
+		//local varaible
+		int a=30;
+		System.out.println(b);
+		System.out.println(a);
+		
+
+	}
+
+}
