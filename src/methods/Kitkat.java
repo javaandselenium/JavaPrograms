45a63615916b7115106d8a9f0fd02e4f03becaed
QSP <QSP@DESKTOP-EVK9GMC>
2020-11-03 09:23:34 +0530

    Methodoveriding

---
+++ src/methods/Kitkat.java
@@ -0,0 +1,11 @@
+package methods;
+
+public class Kitkat {
+	
+	public void camera()
+	{
+		System.out.println("running backcamera");
+	}
+	
+
+}
---
+++ src/methods/Lolipop.java
@@ -0,0 +1,16 @@
+package methods;
+
+public class Lolipop extends Kitkat{
+	
+	public void camera()
+	{
+		System.out.println("running frontCmaera");
+		super.camera();
+	}
+	public static void main(String[] args) {
+		Lolipop l=new Lolipop();
+		l.camera();
+		
+	}
+
+}
